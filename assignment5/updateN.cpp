f4168d1407ec2879c7b913d59d82c59058678416
manupa <manupakaru@gmail.com>
2016-10-03 22:13:21 +0800

    ++updateName

--- assignment5/updateN.cpp
+++ assignment5/updateN.cpp
@@ -1,5 +1,6 @@
 #include "updateN.h"
 #include <assert.h>
+#include <string>
 
 
 uint64 updateN(OneEvent* event, uint64 word){
@@ -28,11 +29,16 @@ uint64 updateN(OneEvent* event, uint64 word){
 }
 
 int updateName(OneEvent* event, 
-	           uint64 word, 
+	           std::vector<uint64> word, 
 	           std::map<uint32,std::map<uint32,const char *> >* nameMap){
 
+	std::string name;
+
 	if(event->type == 0x100){
-		uint64 hashNumber = word & 0x000000000000ffff;
+		assert(word.size() > 0);
+		uint64 N = (word[0] >> 32) & 0x0000000000000fff;
+		uint64 hashNumber = word[0] & 0x000000000000ffff;
+		uint64 expectedWordSize = (N & 0xf0) >> 4;
 		switch(event->number){
 			case 0x2 :
 			case 0x4 :
@@ -41,11 +47,34 @@ int updateName(OneEvent* event,
 			case 0xC :
 			case 0x3 :
 			case 0x1 :
+				assert(word.size() == expectedWordSize);
+				for (int i = 1; i < expectedWordSize; ++i)
+				{
+					for (int j = 0; j < 8; ++j)
+					{
+						char currName = word[i] >> (j*8);
+						if(currName == 0){
+							assert(i == expectedWordSize - 1);
+							break;
+						} 
+						else{
+							name.push_back(currName);
+						}
+					}
+				}
+
+				event->name = name.c_str();
+
+				if(nameMap != NULL){
+					(*nameMap)[event->number][hashNumber] = name.c_str();
+				}
+
 				//(*nameMap)[event->number][hashNumber]
 
 				break;
 			default :
 				assert(false);
+				break;
 		}
 
 		return 0;
--- assignment5/updateN.h
+++ assignment5/updateN.h
@@ -4,11 +4,12 @@
 #include "common.h"
 #include "basetypes.h"
 #include <map>
+#include <vector>
 #include <cstddef>
 
 uint64 updateN(OneEvent* event, uint64 word);
 int updateName(OneEvent* event, 
-	           uint64 word, 
+	           std::vector<uint64> word, 
 	           std::map<uint32,std::map<uint32,const char *> >* = NULL);
 
 #endif	// __UPDATEN_H__
