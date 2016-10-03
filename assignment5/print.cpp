8a95759c63a7823ec2c744f754314469d55cffb7
manupa <manupakaru@gmail.com>
2016-10-04 01:15:24 +0800

    tabs and cycle count fix

--- assignment5/print.cpp
+++ assignment5/print.cpp
@@ -8,29 +8,29 @@
 using namespace std;
 
 int print_entry(OneEvent* event, std::map<uint32,std::map<uint32,const char *> >* nameMap){
-	cout << "t=" << event->t << ",";
-	cout << "duration=" << "N/A" << ",";
-	cout << "cpu=" << event->cpu << ",";
+	cout << "dt=" << (event->t << 8) << " cycles,\t";
+	cout << "duration=" << "N/A" << ",\t";
+	cout << "cpu=" << event->cpu << ",\t";
 
 	const char* tempStr;
 
 	if(nameMap->find(event->type) != nameMap->end()){
 		if(nameMap[event->type].find(event->number) != nameMap[event->type].end()){
 			tempStr = (*nameMap)[event->type][event->number];
-			cout << "typeName=" << tempStr << ",";
+			cout << "typeName=" << tempStr << ",\t";
 		}
 		else{
-			cout << "type=" << event->type << ",";
-			cout << "number=" << event->number << ",";
+			cout << "type=" << event->type << ",\t";
+			cout << "number=" << event->number << ",\t";
 		}
 	}
 	else{
-		cout << "type=" << event->type << ",";
-		cout << "number=" << event->number << ",";
+		cout << "type=" << event->type << ",\t";
+		cout << "number=" << event->number << ",\t";
 	}
 
-	cout << "rpcid=" << "N/A" << ",";
-	cout << "arg0=" << event->arg0 << ",";
-	cout << "return_t=" << event->return_t << ",";
+	cout << "rpcid=" << "N/A" << ",\t";
+	cout << "arg0=" << event->arg0 << ",\t";
+	cout << "return_t=" << event->return_t << ",\t";
 	cout << "retval=" << event->retval << endl;
 }
