88b9de9aac4d4a86aa715aa628ef0fe792d4ef3d
Sunimal Rathnayake <sunimalr@gmail.com>
2016-11-03 00:11:04 +0800

    merge1

--- plotter/filereader.js
+++ plotter/filereader.js
@@ -6,20 +6,21 @@ function processCoordinates(eventList){
 	var end=eventList[eventList.length-1]-startTime;
 	var verticalpadding=0;
 	
-	
-	eventList.push([1,480,100,0,"line"]);
+
+	//eventList.push([1,480,100,0,"line"]);
 	for (i = 0; i < eventList.length; i++) {
 		switch(eventList[i][0]) {
     		case 0:
+    			verticalpadding=100;
         		break;
     		case 1:
-        		verticalpadding=100;
-        		break;
-        	case 2:
         		verticalpadding=200;
         		break;
-        	case 3:
+        	case 2:
         		verticalpadding=300;
+        		break;
+        	case 3:
+        		verticalpadding=400;
         		break;
 		}
 		eventList[i][1]-=startTime;
@@ -30,4 +31,16 @@ function processCoordinates(eventList){
 
 	return eventList;
 
+}
+
+//inputlist element [cpu,start,end,duration,hex,name]
+//eventlist element [cpu,x,y,duration,hex,name]
+function generateCoordinates(inputlist){
+	var eventlist=[];
+	for (i = 0; i < inputlist.length; i++) {
+		inputlist[i][1]=(inputlist[i][1]/100)*hundredNanotoPixel;
+		inputlist[i][2]=0;	
+		inputlist[i][3]=(inputlist[i][1]/100)*hundredNanotoPixel;
+	}
+	return inputlist;
 }
--- plotter/plotscript.js
+++ plotter/plotscript.js
@@ -6,9 +6,10 @@ var smallBoxWidth = 20;
 var innerBoxWidthRatio = 0.4; // in a two color box, width(innerBox) = 0.4 * width(outerBox).
 var cpuLineMargin = 50; // the blank spaces between two cpu lines.
 var maxLength = 2000;
-var lineheight=2;
-var horizontalpadding=100;
-
+ lineheight=2;
+var horizontalpadding=200;
+var hundredNanotoPixel=5;
+//100 nanosecond = 5 pixels
 
 // Generate a event box. offsetTop is added to the vertical absolute position of the box.
 function appendEvent(event) {
@@ -27,28 +28,21 @@ function plot(entries){
 
 	var content = "<svg style=\"width:" + maxLength + "; height:" + (bigBoxWidth + cpuLineMargin) * 5 + "\">";
 	var events0 = [[0,100,100,"line"],[100,100,50,"smallBox"],[150,100,100,"line"],[250,100,2,"line"],[252,100,8,"smallBox"], [260,100,40,"bigBox"], [300, 100,50,"line"],[350,100,100,"smallBox"],[450,100,50,"bigBox"],[500,100,50,"line"]]
-	var events1 = [[1,100,0,100,"lline"],[0,200,0,30,"linde"],[1,250,0,100,"lihne"],[1,350,0,30,"lihne"],[1,380,0,100,"lines"]]
-	var events0 = [[0,100,0,10,"lines"],[0,200,0,30,"lihne"],[0,250,0,100,"linhe"],[0,350,0,30,"lkne"],[0,380,0,100,"lines"]]
+	var events1 = [[1,100,0,100,0x45,"lline"],[0,200,0,30,0x36,"linde"],[1,250,0,100,0x23,"lihne"],[1,350,0,30,0xd,"lihne"],[1,380,0,10,0xc,"lines"]]
+	var events0 = [[0,100,0,10,0x4,"lines"],[0,200,0,30,0x23,"lihne"],[0,250,0,100,0x33,"linhe"],[0,350,0,30,0x12,"lkne"],[0,380,0,100,0x34,"lines"]]
 	//,[252,100,8,"smallBox"], [260,100,40,"bigBox"], [300, 100,50,"line"],[350,100,100,"smallBox"],[450,100,50,"bigBox"],[500,100,50,"line"]]
+
 
 	events0=processCoordinates(events0);
 	events1=processCoordinates(events1);
 
 	line0=events0[0];
-	//line0[1]=horizontalpadding;
-	//line0[3]=events0[events0.length-1][1];
 	content+=drawLine(line0[1],line0[2]+20,events0[events0.length-2][1],lineheight);
 
 	line1=events1[0];
-	//line1[1]=horizontalpadding;
-	//line1[3]=events1[events1.length-1][1];
 	content+=drawLine(line1[1],line1[2]+20,events1[events1.length-2][1],lineheight);
 
 
-	//var events1 = [[0,50,"line"],[50,150,"smallBox"],[150,250,"bigBox"],[250,252,"line"],[252,278,"smallBox"], [278, 300, "bigBox"], [300, 350, "line"],[350,450,"smallBox"],[450,452,"bigBox"],[452,466,"line"],[466,468,"bigBox"],[468,1252,"line"]]
-	//var events2 = [[0,200,"line"],[200,250,"bigBox"],[250,252,"line"],[252,283,"smallBox"], [283, 300, "bigBox"], [300, 340, "line"],[340,440,"smallBox"],[440,550,"bigBox"],[550,1150,"smallBox"],[1150,1252,"line"]]
-	//var events3 = [[0,1000,"line"],[1000,1150,"bigBox"],[1150,1252,"line"]]
-	// Draw the CPU 0
 
 	for (i = 0; i < events0.length; i++) {
 	    // Append a event at the end of CPU 0, vertical position offset = 0 * one line.
@@ -58,29 +52,17 @@ function plot(entries){
 	    // Append a event at the end of CPU 0, vertical position offset = 0 * one line.
 		content += appendEvent(events1[i]);
 	}
-	//for (i = 0; i < events1.length; i++) {
-	    // Append a event at the end of CPU 0, vertical position offset = 0 * one line.
-	//	content += appendEvent(events1[i]);
-	//}
-
-	// Draw the CPU 1
-	// for (i = 0; i < events1.length; i++) {
-	//     // Append an event at the end of CPU 1, vertical position offset = 1 * one line.
-	// 	content += appendEvent(events1[i][0], events1[i][1], events1[i][2], bigBoxWidth + cpuLineMargin);
-	// }
-	// // Draw the CPU 2
-	// for (i = 0; i < events2.length; i++) {
-	//     // Append an event at the end of CPU 2, vertical position offset = 2 * one line.
-	// 	content += appendEvent(events2[i][0], events2[i][1], events2[i][2], 2 * (bigBoxWidth + cpuLineMargin));
-	// }
-	// // Draw the CPU 3
-	// for (i = 0; i < events3.length; i++) {
-	//     // Append an event at the end of CPU 3, vertical position offset = 3 * one line.
-	// 	content += appendEvent(events3[i][0], events3[i][1], events3[i][2], 3 * (bigBoxWidth + cpuLineMargin));
-	//}
+	
 	// Draw the vertical white time tag line.
 	for (i = 0; i < maxLength; i += seperatorDistance)
 		content += drawTicks(i, 100, seperatorWidth,40);
+	for (i = 0; i < maxLength; i += seperatorDistance)
+		content += drawTicks(i, 200, seperatorWidth,40);
+	for (i = 0; i < maxLength; i += seperatorDistance)
+		content += drawTicks(i, 300, seperatorWidth,40);
+	for (i = 0; i < maxLength; i += seperatorDistance)
+		content += drawTicks(i, 400, seperatorWidth,40);
+
 	content += "</svg>"; 
 	document.getElementById("plotSVG").innerHTML = content;
 }
